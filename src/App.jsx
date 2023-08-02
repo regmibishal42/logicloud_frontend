088cd57bad3e8cc2ede175587f51ac46986fc862
Bishal Regmi <regmibishal42@gmail.com>
2023-08-02 15:20:36 +0545

    feat: Login Page Design MUI

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,35 @@ import { useMemo } from "react";
 import { themeSettings } from "./themes";
 import { BrowserRouter,Routes,Route,Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Layout from "./pages/Dashboard/Layout";
-
+import Layout from "./pages/Layout";
+import Login from "./pages/Auth/login";
+import Signup from "./pages/Auth/Signup";
+// React Query
+import { QueryClient,QueryClientProvider } from "react-query";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const queryClient = new QueryClient();
   return (
    <div>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
       <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            {/* <Route element={<Layout />}> */}
+            <Route element={<Login />}>
+              <Route path="/" element={<Navigate to="/login" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
             </Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+             
           </Routes>
       </ThemeProvider>
       </BrowserRouter>
+    </QueryClientProvider>
    </div>
   )
 }
---
+++ src/pages/Auth/login.jsx
@@ -0,0 +1,131 @@
+
+import {Button,Container,Typography,Grid,Checkbox,Paper,Box,Link,TextField,CssBaseline,FormControlLabel} from "@mui/material";
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+  return (
+    <div>
+    <Container component="main" maxWidth="lg" sx={{borderRadius:10,marginTop:10}}>
+      <Box
+        sx={{
+          marginTop: 0,
+        }}
+      >
+        <Grid container>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage: "url(https://source.unsplash.com/random)",
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderBottomLeftRadius:8,
+              borderTopLeftRadius:8,
+            }}
+          />
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={5}
+            component={Paper}
+            elevation={6}
+            square
+            sx={{
+              borderBottomRightRadius:8,
+              borderTopRightRadius:8,
+            }}
+          >
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h1" variant="h3" sx={{marginTop:1,marginBottom:5}}>
+                LogiCloud
+              </Typography>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="/signup" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+    </div>
+  )
+}
+
+export default Login
+
+
