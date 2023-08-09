e3531410a90b08b9bb3f120ed52d2bd79b9c0a98
Bishal Regmi <regmibishal42@gmail.com>
2023-08-09 16:14:53 +0545

    feat:Forget and Reset Password route

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { getToken } from '../../utils/token';
 
 const Dashboard = () => {
+  const token  = getToken()
+  const navigate = useNavigate();
+  useEffect(()=>{
+    const token  = getToken()
+    if (token == null){
+      navigate('/login')
+    }
+  },[])
   return (
     <div>
       Dashboard
