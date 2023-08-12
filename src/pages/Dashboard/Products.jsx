b3b39f3e6a16b983d98869d301c41f8468ce1c46
Bishal Regmi <regmibishal42@gmail.com>
2023-08-12 12:35:45 +0545

    feat:Custom Header Component

--- src/pages/Dashboard/Products.jsx
+++ src/pages/Dashboard/Products.jsx
@@ -1,11 +1,12 @@
 import React,{useState} from 'react';
-import {} from "@mui/material";
+import {Box} from "@mui/material";
+import Header from '../../components/Header';
 
 const Products = () => {
   return (
-    <div>
-      Products
-    </div>
+    <Box>
+      <Header title="Products" subtitle="See your products here" />
+    </Box>
   )
 }
 
